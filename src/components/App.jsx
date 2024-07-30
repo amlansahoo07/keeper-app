69e7b08a95a0d6cf93a19287a60f9dbb64a09753
Amlan <sahoo.amlanranjan@gmail.com>
2024-07-30 23:38:24 +0530

    Add props and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Heading from "./Header";
+import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-    return (<div>
-        <Heading></Heading>
-        <Note></Note>
-        <Footer></Footer>
-    </div>)
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => 
+        <Note key={note.key} title={note.title} content={note.content} />
+      )}
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function Heading() {
-   return (<header>
+function Header() {
+  return (
+    <header>
       <h1>Keeper</h1>
-      </header>);
+    </header>
+  );
 }
 
-export default Heading
+export default Header;
